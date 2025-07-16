084cd4f1ff5c7be16d30497066f77245738ce289
Samluiz2025 <Udobasamuel5@gmail.com>
2025-07-16 17:34:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -209,3 +209,13 @@ function revealOnScroll() {
 }
 
 window.addEventListener("scroll", revealOnScroll);
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu");
+
+  if (hamburger && menu) {
+    hamburger.addEventListener("click", () => {
+      menu.classList.toggle("open");
+    });
+  }
+});
